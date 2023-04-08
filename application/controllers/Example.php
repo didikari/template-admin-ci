5c3ed5524f53627536cbed6c0a79d1abe48669b4
Didik Ariyanto <aridev@Didiks-MacBook-Pro.local>
2023-04-08 12:30:24 +0700

    remove

--- application/controllers/Example.php
+++ application/controllers/Example.php
@@ -4,8 +4,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Example extends CI_Controller{
     public function index()
     {
-        $data = $this->db->get('barang')->result();
-        var_dump($data); die;
         $data['title'] = 'Dashboard';
         $this->load->view('_layouts/header', $data);
         $this->load->view('_layouts/sidebar', $data);
